3fd6ce65ea8cca464e43aceeb71dd71ec88a7bed
Quynh Do <dndquynh@gmail.com>
2020-09-07 02:11:06 -0700

    Updates

---
+++ precache-manifest.0f5aca07aa61e72c4aebc1578c8f2f0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e1c844d7a28cdd8552e86be9345463",
+    "url": "/shopify-challenge/index.html"
+  },
+  {
+    "revision": "2821ceff33d867277354",
+    "url": "/shopify-challenge/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7d336d96cec631714934",
+    "url": "/shopify-challenge/static/css/main.a85e269b.chunk.css"
+  },
+  {
+    "revision": "2821ceff33d867277354",
+    "url": "/shopify-challenge/static/js/2.d3b5b708.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shopify-challenge/static/js/2.d3b5b708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d336d96cec631714934",
+    "url": "/shopify-challenge/static/js/main.cf2cefb8.chunk.js"
+  },
+  {
+    "revision": "3c5db14417251e347853",
+    "url": "/shopify-challenge/static/js/runtime-main.9ec66774.js"
+  }
+]);
